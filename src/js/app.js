64f54194984a74f8bd23ea5adf7bda49da471682
maria-namira <63401949+maria-namira@users.noreply.github.com>
2022-01-14 11:42:09 +0300

    g

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import data from './data';
-import Table from './Table';
-
-const table = new Table(data);
-
-table.bindToDOM(document.getElementById('container'));
-table.drawTable();
-table.startSorting();
